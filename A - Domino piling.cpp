b937095cc6cdb8776d257413471a84a8097a1044
Tarek Alabd <tarekalabd1@gmail.com>
2020-03-15 11:47:26 +0200

    A - Domino piling.cpp

---
+++ A - Domino piling.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+ 
+using namespace std;
+ 
+int main()
+{
+    int m,n,area, num=0;
+    cin>>m>>n;
+    area = m*n;
+    num = area / 2;
+    cout << num << endl;
+    return 0;
+}
