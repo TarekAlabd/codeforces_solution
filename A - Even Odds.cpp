8ea58234585eda58e5a49ee347900cd739f5ecaf
Tarek Alabd <tarekalabd1@gmail.com>
2020-04-12 17:42:00 +0200

    A - Even Odds

---
+++ A - Even Odds.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   long long n, k;
+   cin >> n >> k;
+   if (k <= (n+1)/2)
+    cout << 2 * k - 1 << endl;
+   else
+    cout << 2 * (k- (n+1)/2) << endl;
+   return 0;
+}
